9259c6667e80dd953879f04fcb88db3a0f45bf28
qisfj <813011968@qq.com>
2022-05-02 01:15:25 +0800

    feat(run): check

--- run/until_test.go
+++ run/until_test.go
@@ -36,3 +36,31 @@ func TestEachUntilImmediately(t *testing.T) {
 	close(stopCh)
 	require.Equal(t, 5, count) // 0, 50, 100, 150, 200
 }
+
+func TestCheckUntil(t *testing.T) {
+	before := time.Now()
+	count := 0
+	go func() {
+		_ = CheckUntil(func() bool {
+			t.Logf("run at %s", time.Since(before)/time.Millisecond*time.Millisecond)
+			count++
+			return count == 4
+		}, 50*time.Millisecond, nil)
+	}()
+	time.Sleep(220 * time.Millisecond)
+	require.Equal(t, 4, count) // 50, 100, 150, 200
+}
+
+func TestCheckUntilImmediately(t *testing.T) {
+	before := time.Now()
+	count := 0
+	go func() {
+		_ = CheckUntilImmediately(func() bool {
+			t.Logf("run at %s", time.Since(before)/time.Millisecond*time.Millisecond)
+			count++
+			return count == 4
+		}, 50*time.Millisecond, nil)
+	}()
+	time.Sleep(170 * time.Millisecond)
+	require.Equal(t, 4, count) // 0, 50, 100, 150
+}
